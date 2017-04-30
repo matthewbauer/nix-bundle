abc23a5dfe1568c3ca1fa84ca8d68e576721acf6
Matthew Bauer <mjbauer95@gmail.com>
2017-04-30 16:15:04 -0500

    Remove closure.*
    Wasn’t working with latest tree changes and not used by anything else.

--- closure.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-target="$1"
-
-expr="with import <nixpkgs> {}; with import ./. {}; closure { targets = [$target]; }"
-
-nix-store --no-gc-warning -r $(nix-instantiate --no-gc-warning -E "$expr")
