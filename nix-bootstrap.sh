7f0418d5787e29783f9bf82b9c071dc3e032ea9b
Matthew Bauer <mjbauer95@gmail.com>
2017-08-21 09:22:48 -0500

    Configurable nixdir location

--- nix-bootstrap.sh
+++ nix-bootstrap.sh
@@ -10,19 +10,23 @@ fi
 proot=`dirname $0`/proot-`uname -p`
 export PROOT_NO_SECCOMP=1
 
+nixdir=$HOME/.nix
+
+OLD_NIX_PATH=$NIX_PATH
 if [ -e $HOME/.nix-profile/etc/profile.d/nix.sh ]; then . $HOME/.nix-profile/etc/profile.d/nix.sh; fi
+if ! [ -z "$OLD_NIX_PATH" ]; then NIX_PATH="$OLD_NIX_PATH"; fi
 
 if [ -z "$IN_PROOT" ]; then
     export IN_PROOT=1
 
-    if ! [ -d $HOME/.nix ]; then
-        mkdir -p $HOME/.nix
+    if ! [ -d $nixdir ]; then
+        mkdir -p $nixdir
         s=$(mktemp)
         curl https://nixos.org/nix/install -o $s
-        $proot -b $HOME/.nix:/nix $0 sh $s
+        $proot -b $nixdir:/nix $0 sh $s
     fi
 
-    $proot -b $HOME/.nix:/nix $0 $cmd
+    $proot -b $nixdir:/nix $0 $cmd
 
     export IN_PROOT=
     exit
