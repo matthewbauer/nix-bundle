45cfa4b2bb294e489ac17b396836ad2cb7d4e568
Matthew Bauer <mjbauer95@gmail.com>
2017-10-13 14:57:42 -0500

    Add release.nix

---
+++ release.nix
@@ -0,0 +1,34 @@
+{ nixpkgs ? <nixpkgs>
+, nixpkgs' ? import nixpkgs {}}: with nixpkgs';
+
+stdenv.mkDerivation rec {
+  pname = "nix-bundle";
+  name = "${pname}-${version}";
+  version = "0.2.0";
+
+  src = ./.;
+
+  # coreutils, gnutar is actually needed by nix for bootstrap
+  buildInputs = [ nix coreutils makeWrapper gnutar gzip bzip2 ];
+
+  nixBundlePath = lib.makeBinPath [ nix coreutils gnutar gzip bzip2 ];
+  nixRunPath = lib.makeBinPath [ nix coreutils ];
+
+  makeFlags = [ "PREFIX=$(out)" ];
+
+  postInstall = ''
+    mkdir -p $out/bin
+    makeWrapper $out/share/nix-bundle/nix-bundle.sh $out/bin/nix-bundle \
+      --prefix PATH : ${nixBundlePath}
+    makeWrapper $out/share/nix-bundle/nix-run.sh $out/bin/nix-run \
+      --prefix PATH : ${nixRunPath}
+  '';
+
+  meta = with lib; {
+    maintainers = [ maintainers.matthewbauer ];
+    platforms = platforms.all;
+    description = "Create bundles from Nixpkgs attributes";
+    license = licenses.mit;
+    homepage = https://github.com/matthewbauer/nix-bundle;
+  };
+}
