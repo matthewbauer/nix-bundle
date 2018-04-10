d17fcd590c9a7233eaf50c5fc6e43250f41e9339
Matthew Bauer <mjbauer95@gmail.com>
2018-04-10 15:55:58 -0500

    cleanup nix override

--- nix.nix
+++ nix.nix
@@ -3,13 +3,19 @@ with import ./default.nix {};
 
 nix-bootstrap-path {
   run = "/bin/nix";
-  target = nix.overrideAttrs (o: {
-    buildInputs = [ curl openssl sqlite libseccomp ];
-    doCheck = false;
-    preConfigure = (o.preConfigure or "") + ''
-      NIX_CFLAGS_COMPILE+=" -g0"
-      configureFlagsArray+=(--with-sandbox-shell=${bash}/bin/bash)
-    '';
-    propagatedBuildInputs = [gnutar bzip2 xz gzip coreutils bash boehmgc];
-  });
+  target = buildEnv {
+    name = "nix-bundle";
+    paths = [
+    coreutils bzip2 xz gnutar coreutils bash
+    (nix.overrideAttrs (o: {
+      buildInputs = [ curl openssl sqlite libseccomp ];
+      doCheck = false;
+      preConfigure = (o.preConfigure or "") + ''
+        NIX_CFLAGS_COMPILE+=" -g0"
+        configureFlagsArray+=(--with-sandbox-shell=${bash}/bin/bash)
+      '';
+      propagatedBuildInputs = [gnutar bzip2 xz gzip coreutils bash boehmgc];
+    }))
+    ];
+  };
 }
