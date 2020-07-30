1bffaa07f337c7329ca13cfe4ec00c4829f1d5d7
Matthew Bauer <mjbauer95@gmail.com>
2020-07-30 10:44:57 -0500

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "The purely functional package manager";
+
+  inputs.nixpkgs.url = "nixpkgs/nixos-20.03-small";
+
+  outputs = { self, nixpkgs }: let
+    systems = [ "x86_64-linux" "i686-linux" "x86_64-darwin" "aarch64-linux" ];
+    forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+  in {
+    exporters = forAllSystems (system: let
+      nixpkgs' = nixpkgs.legacyPackages.${system};
+      nix-bundle = import self { nixpkgs = nixpkgs'; };
+    in {
+      nix-bundle = { program }: let
+        script = nixpkgs'.writeScript "startup" ''
+        #!/bin/sh
+        .${nix-bundle.nix-user-chroot}/bin/nix-user-chroot -n ./nix -- ${program} $@
+        '';
+      in nix-bundle.makebootstrap {
+        targets = [ script ];
+        startup = ".${builtins.unsafeDiscardStringContext script} '\"$@\"'";
+      };
+    });
+
+    defaultExporter = forAllSystems (system: self.exporters.${system}.nix-bundle);
+  };
+}
